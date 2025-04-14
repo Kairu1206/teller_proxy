b9217c77146f5f0b394eaa0eeba2560818f1d6bc
Q <kylenguyen1206@gmail.com>
2025-04-14 16:22:08 -0400

    update

--- server.js
+++ server.js
@@ -34,49 +34,60 @@ app.post('/teller-proxy', async (req, res) => {
   .then(response => {
     console.log('Response:', response.data);
     const accounts = [];
-    for (let i = 0; i < response.data.length; i++) {
-      const acc_last_four = response.data[i].last_four;
-      const acc_subtype = response.data[i].subtype; //Subtype: Checking, Savings, Credit Card
-      const acc_institution = response.data[i].institution[0]; //Name of the institution
-      const acc_currency = response.data[i].currency; //Currency of the account
-      const acc_type = response.data[i].type; //Type: Debit, Credit
-      const acc_status = response.data[i].status; //Status: Open, Closed
-      const acc_name = response.data[i].name; //Name of the account
-      const acc_id = response.data[i].id; //Account ID
-  
-      const getBalance = async (acc_id, accessToken) => {
-        axios.get(`https://api.teller.io/accounts/${acc_id}/balances`, {
-          auth: {
-            username: accessToken,
-            password: ''
-          }
-        })
-        .then(balance_response => {
-          console.log('Balance Response:', balance_response.data);
-          console.log("Available Balance:", balance_response.data.available);
-          return balance_response.data.available;
-        })
-        .catch(balance_error => {
-          console.error('Error:', balance_error);
-        });
-      };
-      
-      let acc_balance = getBalance(acc_id, accessToken);
-      console.log("Acc Balance:", acc_balance);
-      
+    let acc_last_four;
+    let acc_subtype;
+    let acc_institution;
+    let acc_currency;
+    let acc_type;
+    let acc_status;
+    let acc_name;
+    let acc_id;
 
-      accounts.push({
-        acc_last_four,
-        acc_subtype,
-        acc_institution,
-        acc_currency,
-        acc_type,
-        acc_status,
-        acc_name,
-        acc_balance
+    for (let i = 0; i < response.data.length; i++) {
+      acc_last_four = response.data[i].last_four;
+      acc_subtype = response.data[i].subtype; //Subtype: Checking, Savings, Credit Card
+      acc_institution = response.data[i].institution[0]; //Name of the institution
+      acc_currency = response.data[i].currency; //Currency of the account
+      acc_type = response.data[i].type; //Type: Debit, Credit
+      acc_status = response.data[i].status; //Status: Open, Closed
+      acc_name = response.data[i].name; //Name of the account
+      acc_id = response.data[i].id; //Account ID
+    }
+
+    async function getBalance(acc_id, accessToken)
+    {
+      axios.get(`https://api.teller.io/accounts/${acc_id}/balances`, {
+        auth: {
+          username: accessToken,
+          password: ''
+        }
+      })
+      .then(balance_response => {
+        console.log('Balance Response:', balance_response.data);
+        console.log("Available Balance:", balance_response.data.available);
+        return balance_response.data.available;
+      })
+      .catch(balance_error => {
+        console.error('Error:', balance_error);
+      });
+    };
+    
+    let acc_balance = getBalance(acc_id, accessToken)
+      .then(balance => {
+        console.log("Available Balance:", balance);
+        accounts.push({
+          acc_last_four: acc_last_four,
+          acc_subtype: acc_subtype,
+          acc_institution: acc_institution,
+          acc_currency: acc_currency,
+          acc_type: acc_type,
+          acc_status: acc_status,
+          acc_name: acc_name,
+          acc_id: acc_id,
+          acc_balance: balance
+        });
       });
 
-    }
     return res.json(accounts);
   })
   .catch(error => {
