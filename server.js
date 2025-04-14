2eee03c2a7ce36aaa61a9aadcb811348dcd0d886
Q <kylenguyen1206@gmail.com>
2025-04-14 11:17:19 -0400

    update proxy

--- server.js
+++ server.js
@@ -15,13 +15,37 @@ const key = fs.readFileSync('./private_key.pem');
 const cert = fs.readFileSync('./certificate.pem');
 
 app.post('/teller-proxy', (req, res) => {
-  const accessToken = req.body;
-  
+  const accessTokenObject = req.body;
+  const accessToken = accessTokenObject.accessToken;
   console.log(accessToken);
 
   if (!accessToken) {
     return res.status(400).json({ error: 'Missing access token' });
   }
+
+  const options = {
+    hostname: 'api.teller.io',
+    path: '/accounts',
+    method: 'GET',
+    key,
+    cert,
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,  // Use the access token in the request headers
+      'App-Id': process.env.APP_ID,
+      'Accept': 'application/json'
+    }
+  };
+
+  const request = https.request(options, (response) => {
+    return res.status(response.statusCode).send(response);
+  });
+
+  request.on('error', (error) => {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  });
+
+  request.end();
 });
 
 app.listen(process.env.PORT, () => {
